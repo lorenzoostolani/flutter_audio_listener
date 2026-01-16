34971e8c08b3a817caa4a0e25e53c355547e6921
lorenzoostolani <lorenzo.ostolani.stud@ispascalcomandini.it>
2026-01-16 08:31:22 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ListenAudio());
+}
+
+class ListenAudio extends StatelessWidget {
+  const ListenAudio({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.green,
+          brightness: Brightness.dark,
+        ),
+      ),
+      home: const Listenerhomepage(),
+    );
+  }
+}
+
+class Listenerhomepage extends StatefulWidget {
+  const Listenerhomepage({super.key});
+
+  @override
+  State<Listenerhomepage> createState() => _ListenerhomepageState();
+}
+
+class _ListenerhomepageState extends State<Listenerhomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Audio listener'),
+        //backgroundColor: Colors.blueAccent,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Image.network(
+              'https://static.vecteezy.com/system/resources/previews/023/986/631/original/whatsapp-logo-whatsapp-logo-transparent-whatsapp-icon-transparent-free-free-png.png',
+              height: 300,
+            ),
+            const Text('Scegli un vocale'),
+          ],
+        ),
+      ),
+    );
+  }
+}
